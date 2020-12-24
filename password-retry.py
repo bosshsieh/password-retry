f5a6ca20e841c3eed3fb61e9271c3cb7f0cd70b7
boss hsieh <bosshsieh717@gmail.com>
2020-12-24 19:57:12 +0800

    optimization

--- password-retry.py
+++ password-retry.py
@@ -1,13 +1,15 @@
 #輸入密碼驗證
-password = input('請輸入密碼: ')
+password = 'a123456'
 x = 3
-while x <= 3 and x > 1:
-	if password == 'a123456':
+while x > 0:
+	x = x - 1
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
 		print('登入成功')
 		break
 	else:
-	 	x = x - 1 
-	 	print('密碼錯誤，還有',x , '次機會')
-	 	password = input('請輸入密碼: ')
- 		
-	print('你輸入超過三次')
+	 	print('密碼錯誤')
+	 	if x > 0:
+	 		print('還有',x , '次機會')
+	 	else:		
+ 			print('你輸入超過三次')
